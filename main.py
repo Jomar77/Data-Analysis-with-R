c69b1a1bb738b4107e0bcb357f9ee9e2f1b49968
JOMAR NACORDA <jnacorda04@gmail.com>
2023-01-18 21:47:55 +1300

    eyo

--- main.py
+++ main.py
@@ -9,3 +9,28 @@ import matplotlib.cm as cm
 
 #read in the data
 data = sp.genfromtxt("web_traffic.tsv", delimiter="\t")
+
+#split the data into x and y
+x = data[:,0]
+y = data[:,1]
+
+#clean the data
+x = x[~sp.isnan(y)]
+y = y[~sp.isnan(y)]
+
+#plot the data
+plt.scatter(x,y)
+plt.title("Web traffic over the last month")
+plt.xlabel("Time")
+plt.ylabel("Hits/hour")
+plt.xticks([w*7*24 for w in range(10)], ['week %i' % w for w in range(10)])
+plt.autoscale(tight=True)
+plt.grid()
+plt.show()
+
+#fit the data with a polynomial
+fp1, residuals, rank, sv, rcond = sp.polyfit(x, y, 1, full=True)
+print ("Model parameters: %s" % fp1)
+print (residuals)
+
+f1 = sp.poly1d(fp1)
