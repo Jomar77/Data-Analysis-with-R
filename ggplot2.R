9c666366caaf338e38d701d9357c13ac5c8d123f
JOMAR NACORDA <jnacorda04@gmail.com>
2023-02-10 13:20:29 +1300

    ggplot2

---
+++ ggplot2.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(ggplot2)
+library(palmerpenguins)
+
+ggplot(data=penguins2)+geom_point(mapping = aes(x=flipper_length_mm, y=body_mass_g))
+
+ggplot(data=penguins2)+geom_point(mapping = aes(x=bill_length_mm, y=bill_depth_mm))
+
+glimpse(penguins2)
