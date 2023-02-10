9451b1768c627bf28ba842bd2fe0475b46017f4f
JOMAR NACORDA <jnacorda04@gmail.com>
2023-02-10 13:52:05 +1300

    facet

--- Facet.r
+++ Facet.r
@@ -2,7 +2,14 @@ library(tidyverse)
 library(palmerpenguins)
 
 # Load the data
-ggplot(data = penguins) +
-  geom_smooth(mapping = aes(x =flipper_length_mm,y=body_mass_g)) +
+ggplot(data = penguins, aes(x =flipper_length_mm,y=body_mass_g)) +
   geom_point(aes(color=species))+
   facet_wrap(~species)
+
+ggplot(data = penguins, aes(x =flipper_length_mm,y=body_mass_g)) +
+  geom_point(aes(color=species))+
+  facet_grid(~sex)
+
+ggplot(data = diamonds) +
+  geom_bar(mapping = aes(x = color, fill = cut)) +
+  facet_wrap(~cut)
